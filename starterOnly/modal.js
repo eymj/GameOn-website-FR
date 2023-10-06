7905e9283328e9298c2342f49ff80b17a9adb7fc
Jeremy <jfouin2@protonmail.com>
2023-10-06 12:01:23 +0200

    feat: modal can be closed #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,23 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const close = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+close.forEach((btn) => btn.addEventListener("click", closeModal));
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
