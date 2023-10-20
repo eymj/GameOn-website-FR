6673847729a79c1a6014c768018af6b9882aef1f
Jeremy <jfouin2@protonmail.com>
2023-10-20 10:17:39 +0200

    feat: form validation #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,8 +10,16 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const submitBtn = document.querySelector(".modal-body .btn-submit");
 const close = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const firstInput = document.getElementById('first');
+const lastInput = document.getElementById('last');
+const emailInput = document.getElementById('email');
+const birthdateInput = document.getElementById('birthdate');
+const quantityInput = document.getElementById('quantity');
+const radioInputs = document.querySelectorAll(`.modal-body input[type="radio"]`);
+const checkboxInput = document.getElementById('checkbox1');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,4 +37,79 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Validate first and last name
+function validateText(field) {
+  const value = field.value;
+  if (value.length >= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+// Validate quantity
+function validateNumber(field) {
+  const value = field.value.trim();
+  if (!isNaN(value)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Validate mail
+function validateEmail(field) {
+  const value = field.value.trim();
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (pattern.test(value)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Validate location checks
+function validateLocations(fields) {
+  for (let i = 0; i < fields.length; i++) {
+      if (fields[i].checked) {
+          return true;
+      }
+  }
+  return false;
+}
+
+// Validate mandatory checkbox
+function validateCheckbox(field) {
+  return field.checked;
+}
+
+// Setup validators
+const fieldsValidator = [
+  () => validateText(firstInput),
+  () => validateText(lastInput),
+  () => validateEmail(emailInput),
+  () => validateNumber(quantityInput),
+  () => validateLocations(radioInputs),
+  () => validateCheckbox(checkboxInput)
+];
+
+// Check if all validators return true
+function validateForm(validators) {
+  for (let validator of validators) {
+      if (!validator()) {
+          return false;
+      }
+  }
+  return true;
+}
+
+// Handle form validation
+function validate(e) {
+
+  const form = e.target;
+  e.preventDefault();
+
+  if (validateForm(fieldsValidator)) {
+    form.reset();
+  }
+}
